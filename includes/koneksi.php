dae954ee90d572a86ec0b4e06a4126a30ce13856
Stepennn <muhlatif1809@gmail.com>
2026-09-21 12:11:20 +0700

    Fix password env variable di koneksi.php

--- includes/koneksi.php
+++ includes/koneksi.php
@@ -1,13 +1,13 @@
 <?php
-$host = "localhost";
-$port = "5432";
-$db   = "rental_ps"; // DIGANTI: sebelumnya "rental"
-$user = "postgres";
-$pass = "postgres"; // ganti dengan password PostgreSQL kamu
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$dbname = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
 
 try {
-    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Koneksi database gagal: " . $e->getMessage());
+    die("Koneksi gagal: " . $e->getMessage());
 }
