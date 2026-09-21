670dbad612bfaf2fd5bf5003e03f0be9a288803d
Stepennn <muhlatif1809@gmail.com>
2026-09-21 12:58:20 +0700

    TEST

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 $page_title = "Beranda";
-require __DIR__ . '/../includes/koneksi.php';
-include __DIR__ . '/../includes/header.php';
+require __DIR__ . '/includes/koneksi.php';
+include __DIR__ . '/includes/header.php';
 
 $totalKonsol = $pdo->query("SELECT COUNT(*) FROM konsol")->fetchColumn(); // DIGANTI: $totalKendaraan -> $totalKonsol, tabel kendaraan -> konsol
 $totalPenyewa = $pdo->query("SELECT COUNT(*) FROM penyewa")->fetchColumn();
@@ -26,4 +26,4 @@ $totalPenyewa = $pdo->query("SELECT COUNT(*) FROM penyewa")->fetchColumn();
                 <p>0</p>
             </article>
         </section>
-<?php include __DIR__ . '/../includes/footer.php'; ?>
+<?php include __DIR__ . '/includes/footer.php'; ?>
