0c2269ee14f9cff625c426bf5864e3940df98f63
Stepennn <muhlatif1809@gmail.com>
2026-09-21 13:15:29 +0700

    konneski

--- api/includes/koneksi.php
+++ api/includes/koneksi.php
@@ -1,9 +1,9 @@
 <?php
 // Mengambil variabel lingkungan (Environment Variables) dari Vercel
-$host     = getenv('DB_HOST') ?: 'db.xxx.supabase.co'; // Ganti dengan Host Supabase Anda
-$port     = getenv('DB_PORT') ?: '5432';
+$host     = getenv('DB_HOST') ?: 'aws-0-ap-south-1.pooler.supabase.com'; // Ganti dengan Host Supabase Anda
+$port     = getenv('DB_PORT') ?: '6543';
 $dbname   = getenv('DB_NAME') ?: 'postgres';
-$user     = getenv('DB_USER') ?: 'postgres';
+$user     = getenv('DB_USER') ?: 'postgres.skrfnclqkknzplzjivql';
 $password = getenv('DB_PASS') ?: 'Muhlatif180906';
 
 try {
